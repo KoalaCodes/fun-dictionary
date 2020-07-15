68bf4ad1513d0282ca5d5eb9eb3ba06ac6446e8d
KoalaKodes <mannymannybanny@gmail.com>
2020-07-14 21:45:38 -0700

    developed more style and prepared an animation

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const $input = $("input[type='text']");
 const $definition = $(".definition");
 const $word = $(".word");
 const $type = $(".type");
-const $exampe = $(".example");
+const $example = $(".example");
 
 $("form").on("submit", grabData);
 
@@ -47,6 +47,6 @@ function render() {
     $word.html(`<h3>${dictionaryData.word}</h3>`);
     $definition.html(`<h4>${dictionaryData.definitions[0].definition}</h4>`);
     $type.html(`<h3>${dictionaryData.definitions[0].type}</h3>`);
-    $exampe.html(`<h3>${dictionaryData.definitions[2].example}</h3>`);
+    $example.html(`<h3>${dictionaryData.definitions[2].example}</h3>`);
     userInput = $input.val(""); // this line will reset the value in the input bar.
 };
