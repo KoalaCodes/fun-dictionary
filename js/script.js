3846325aa02669e002dd23d3a42c6c6e0fcca9b6
KoalaKodes <mannymannybanny@gmail.com>
2020-07-15 20:54:41 -0700

    added more styling and a randomnizer for alphabet in js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,18 @@ const $definition = $(".definition");
 const $word = $(".word");
 const $type = $(".type");
 const $example = $(".example");
+const $alphabet = $(".alphabet"); // hoping to grab the alphabet in the background and change color
+
 
 $("form").on("submit", grabData);
 
 
 
 
+
+$alphabet.html(`<p class="alpha">${randomAlphabet(26)}</p>`);
+
+const $color = $(".alpha")
 
 
 
@@ -31,7 +37,7 @@ function grabData(event) { // This line will use the ajax method to retrieve dat
             dictionaryData = data;
             console.log("data", data);
             render();
-            
+            changeColor();
         },
         (error) => {
             console.log("error", error);
@@ -44,9 +50,31 @@ function grabData(event) { // This line will use the ajax method to retrieve dat
 
 
 function render() {
-    $word.html(`<h3>${dictionaryData.word}</h3>`);
+    $word.html(`<h2>${dictionaryData.word}</h2>`);
     $definition.html(`<h4>${dictionaryData.definitions[0].definition}</h4>`);
     $type.html(`<h3>${dictionaryData.definitions[0].type}</h3>`);
     $example.html(`<h3>${dictionaryData.definitions[2].example}</h3>`);
     userInput = $input.val(""); // this line will reset the value in the input bar.
+     // this line changes the color of the selected items
+    
 };
+
+function changeColor() {
+    $color.on("submit", $color.css("color", "gold"));  // this line changes the color of the selected items
+};
+
+function randomAlphabet(len) {
+    var text = "";
+    
+    var charset = "A:B 'C D ;E *F xG ßH αI öJ K ΩL M N O P Q R S T U V W X Y Z";
+    
+    for (var i = 0; i < len; i++)
+      text += charset.charAt(Math.floor(Math.random() * charset.length));
+    
+    return text;
+  };
+  
+//   function example() {
+//       for(let i = 0; i < dictionaryData.)
+//   }
+  
