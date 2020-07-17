4c7971a8006a88ec7140fe274ea3fc08a4e29157
KoalaKodes <mannymannybanny@gmail.com>
2020-07-16 19:29:53 -0700

    finalized style and functionality

---
+++ README.md.md
@@ -0,0 +1,25 @@
+## In this Project I set out to use the jQuery library and AJAX method to work with a dictionary
+## api. 
+
+	In this project I demonstrate profiency not only with jQuery but with css as well.
+this project dependant. 
+
+	the first steps were to establish structure in html. once that was set up you can develop the constants in jQuery.
+
+	then develop the the functionality.
+
+ below is the wireframe I drew.
+I strayed from the wire frame but happy with how it ended up.
+
+![screenshot](https://imgur.com/UtdsA4x)
+
+this is final result.
+
+![finalresult](https://imgur.com/STvCx72)
+
+
+goals accomplished: highly stylized webpage. minimal look. api data retrieval. functional apps.
+
+
+Future Goals: to personalize the website a bit further. adding memory storage maybe even a game
+where people who learn words. rankings.profiles.
--- js/script.js
+++ js/script.js
@@ -7,12 +7,17 @@ const $word = $(".word");
 const $type = $(".type");
 const $example = $(".example");
 const $alphabet = $(".alphabet"); // hoping to grab the alphabet in the background and change color
+const colors = ["#e8d87b", "#db9688", "#88cfdb", "#c388db"]; // this is an array in order to have a random color be selected
 
+// this line will generate a random
 
 $("form").on("submit", grabData);
 
 
-
+// $(document).keypress(function(event) {
+//     $("h1").text(event.key);
+// });
+  
 
 
 $alphabet.html(`<p class="alpha">${randomAlphabet(26)}</p>`);
@@ -51,19 +56,20 @@ function grabData(event) { // This line will use the ajax method to retrieve dat
 
 function render() {
     $word.html(`<h2>${dictionaryData.word}</h2>`);
-    $definition.html(`<h4>${dictionaryData.definitions[0].definition}</h4>`);
-    $type.html(`<h3>${dictionaryData.definitions[0].type}</h3>`);
-    $example.html(`<h3>${dictionaryData.definitions[2].example}</h3>`);
+    $definition.html(`<h3> Definition : ${dictionaryData.definitions[0].definition}</h3>`);
+    $type.html(`<h3> ${dictionaryData.definitions[0].type}</h3>`);
+    $example.html(`<h3> Example : ${dataIteration()}</h3>`);
     userInput = $input.val(""); // this line will reset the value in the input bar.
      // this line changes the color of the selected items
-    
+     
 };
 
 function changeColor() {
-    $color.on("submit", $color.css("color", "gold"));  // this line changes the color of the selected items
+    let j = (Math.floor(Math.random() * colors.length));
+    $color.on("submit", $color.css("color", colors[j]));  // this line changes the color of the selected items
 };
 
-function randomAlphabet(len) {
+function randomAlphabet(len) {  //this line is will randomize letters into a word
     var text = "";
     
     var charset = "A:B 'C D ;E *F xG ßH αI öJ K ΩL M N O P Q R S T U V W X Y Z";
@@ -74,7 +80,27 @@ function randomAlphabet(len) {
     return text;
   };
   
-//   function example() {
-//       for(let i = 0; i < dictionaryData.)
-//   }
-  
+function dataIteration() {
+    for(let i = 0;i < dictionaryData.definitions.length; i++){
+        if(dictionaryData.definitions[i].example === null) {
+            return "NO EXAMPLE CAN BE FOUND SORRY"
+        } else {
+            return dictionaryData.definitions[i].example;
+        } // return dictionaryData.definitions[i].example
+    }
+};  // this line will itarate through the object to find examples. 
+
+function dataAccess() {
+    for(let i = 0;i <= dictionaryData.definitions.length; i++){
+        return dictionaryData.definitions[i];
+    }
+}; //this line will iterate through the object to find definitions.
+
+// function change (){
+   
+    
+
+//     //this line of code will select a random position in an array to chose a color.
+    
+// };
+
